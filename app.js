bd9be00032ba3e76e9f140b8200f9687f4c125ae
sarahcurrey <sarah.currey@generalassemb.ly>
2019-03-26 15:25:59 -0400

    javascript-refactor

--- app.js
+++ app.js
@@ -1,135 +1,71 @@
-// make sure form enter cannot be empty "" or not include @
-
-// Get the modal
+window.onload = function(){ 
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
+var nextBtn = document.getElementById('nextBtn');
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
+  console.log('this is the btn onclick');
+};
 span.onclick = function() {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
+  console.log('this is the span onlick');
+};
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    console.log('this is the window onclick');
   }
-}
-
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
-
+};
+var currentTab = 0;
 function showTab(n) {
-  // This function will display the specified tab of the form...
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
-  //... and fix the Previous/Next buttons:
   if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
   } else {
     document.getElementById("prevBtn").style.display = "inline";
   }
-  if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
-  }
-  //... and run a function that will display the correct step indicator:
-  fixStepIndicator(n)
+  fixStepIndicator(n);
 }
-
-function nextPrev(n) {
-  // This function will figure out which tab to display
+showTab(currentTab); 
+window.nextPrev =function(n) {
   var x = document.getElementsByClassName("tab");
-  // Exit the function if any field in the current tab is invalid:
   if (n == 1 && !validateForm()) return false;
-  // Hide the current tab:
   x[currentTab].style.display = "none";
-  // Increase or decrease the current tab by 1:
   currentTab = currentTab + n;
-  // if you have reached the end of the form...
   if (currentTab >= x.length) {
-    // ... the form gets submitted:
     document.getElementById("regForm").submit();
     return false;
   }
-  // Else, display the correct tab:
   showTab(currentTab);
-}
-
-//  NEED TO ADD VALIDATION FOR @ in EMAIL
+};
 
 function validateForm() {
-  // This function deals with validation of the form fields
   var x, y, i, valid = true;
   x = document.getElementsByClassName("tab");
   y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
   for (i = 0; i < y.length; i++) {
-    // If a field is empty...
     if (y[i].value == "") {
-      // add an "invalid" class to the field:
       y[i].className += " invalid";
-      // and set the current valid status to false
       valid = false;
     }
   }
-  // If the valid status is true, mark the step as finished and valid:
   if (valid) {
     document.getElementsByClassName("step")[currentTab].className += " finish";
   }
-  return valid; // return the valid status
+  return valid; 
 }
 
 function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
   var i, x = document.getElementsByClassName("step");
   for (i = 0; i < x.length; i++) {
     x[i].className = x[i].className.replace(" active", "");
   }
-  //... and adds the "active" class on the current step:
   x[n].className += " active";
 }
+};
 
-// SHEETSU POST METHOD
-
-// <head>
-//   <script src="//script.sheetsu.com/"></script>
-// </head>
-// <body>
-//   <form id="myForm">
-//     <input type="text" name="first_name">
-//     <input type="text" name="score">
-//     <button type="submit">Save</button>
-//   </form>
-//   <script>
-//     document.querySelector("#myForm").addEventListener("submit", function (e) {
-//       e.preventDefault();
-//       saveData();
-//     });
-//     function saveData() {
-//       var first_name = document.getElementsByName("first_name")[0].value,
-//         score = document.getElementsByName("score")[0].value;
-//       var data = {
-//         name: first_name,
-//         score: score
-//       };
-//       Sheetsu.write("https://sheetsu.com/apis/v1.0dw/020b2c0f/", data, {}, function (result) {
-//         console.log(result);
-//       });
-//     }
-//   </script>
-// </body>
 
 
 
---
+++ questions.js
@@ -0,0 +1,190 @@
+var resultOptions = [
+    {   
+        title: 'Software Engineering Immersive',
+        desc: '<p>Build rich, interactive, full-stack apps with key languages and dive into algorithms, data structures, design patterns, APIs, and more.</p>'},
+
+    {   
+        title: 'User Experience Design Immersive',
+        desc: '<p>The User Experience Design Immersive program is crafted by top practitioners in the field, with a specific focus on helping you transition into a UX design career.</p>'},
+    {   
+        title: 'Data Science Immersive',
+        desc: '<p>Learn the tools and techniques you need to make better decisions through data, and land a job in one of the most sought after fields in tech.</p>'},
+    {   
+        title: 'Digital Marketing Immersive',
+        desc: '<p>Join the high-growth world of digital marketing in our full-time Immersive course. Our expert-designed curriculum is continually updated to keep pace with shifting employer demands. Gain competitive skills, personalized career coaching, and access to resources to continually foster your professional potential.</p>'},
+    {   
+        title: 'Software Engineering Immersive Remote',
+        desc: '<p>Learn online and build rich, interactive, full-stack apps with key languages and dive into algorithms, data structures, design patterns, APIs, and more.</p>'},
+    {   
+        title: 'Python Programming',
+        desc: '<p>Dive into fundamental concepts and techniques, and build your own custom web or data application.</p>'},
+    {   
+        title: 'User Experience Design',
+        desc: '<p>Apply tools like Adobe, Sketch, Illustrator and Invision to create UX documentation for digital products; conduct research to gain insights into customer behavior.</p>'},
+    {   
+        title: 'Data Analytics',
+        desc: '<p>Confidently make — and defend — critical decisions using the results of your data analysis.Collect, clean, and analyze data. Skills, Tools, & Strategies. Use Excel, SQL, and Tableau to spot trends and drive business decisions with real‐world data.</p>'},
+    {
+        title: 'Digital Marketing',
+        desc: '<pBecome a driver of your company\'s bottom line by using cutting‐edge techniques and platforms to market products and acquire users.</p>'
+    },
+    {
+        title: 'Data Science',
+        desc: '<p>Use Python to mine datasets and predict patterns. Build statistical models — regression and classification — that generate usable information from raw data.</p>'
+    },
+    {
+        title: 'Product Management',
+        desc: '<pGain leadership and communication skills to succeed in this in-demand field. Learn to launch viable, market-ready products and features that solve real problems for your customers.</p>'
+    },
+    {
+        title: 'Visual Design',
+        desc: '<p> Apply fundamental tools of visual communication - typography, color and layout - to design beautiful digital products.</p>'
+    },
+    {
+        title: 'Front End Web Development',
+        desc: '<p>Use HTML5, CSS3, JavaScript, jQuery, GitHub, and Sublime in concert. Build a fully responsive, interactive website that stands apart from the usual templates. </p>'
+    },
+    {
+        title: 'JavaScript Development',
+        desc: '<p>Learn to code in JavaScript, the native language of the web used by developers the world over.</p>'
+    },
+    {
+        title: 'React Development',
+        desc: '<p>Start building maintainable web applications that can handle constantly changing data at scale. Expand your programming toolkit, and discover why both developers and employers favor this robust JavaScript library. </p>'
+    },
+    {
+        title: 'Python Programming Remote',
+        desc: '<p>Learn online and discover why professionals ranging from analysts to web developers rely on Python programming to excel in their work. Dive into fundamental concepts and techniques, and build your own custom web or data application.</p>'
+    },
+    {
+        title: 'User Experience Design Remote',
+        desc: '<pLearn online and apply tools like Adobe, Sketch, Illustrator and Invision to create UX documentation for digital products; conduct research to gain insights into customer behavior.</p>'
+    },
+    {
+        title: 'Data Analytics Remote',
+        desc: '<p> Learn online and confidently make — and defend — critical decisions using the results of your data analysis.</p>'
+    },
+    {
+        title: 'Digital Marketing Remote',
+        desc: '<p> Learn online and become a driver of your company\'s bottom line by using cutting‐edge techniques and platforms to market products and acquire users.</p>'
+    },
+    {
+        title: 'Data Science Remote',
+        desc: '<p> Learn online and use Python to mine datasets and predict patterns.</p>'
+    },
+    {
+        title: 'Product Management Remote',
+        desc: '<pLearn online and gain leadership and communication skills to succeed in this in-demand field. Learn to launch viable, market-ready products and features that solve real problems for your customers.</p>'
+    },
+    {
+        title: 'Visual Design Remote',
+        desc: '<p>Learn online and apply fundamental tools of visual communication - typography, color and layout - to design beautiful digital products. </p>'
+    },
+    {
+        title: 'Front End Web Development Remote',
+        desc: '<p> Learn online and use HTML5, CSS3, JavaScript, jQuery, GitHub, and Sublime in concert.</p>'
+    },
+    {
+        title: 'JavaScript Development Remote',
+        desc: '<p> Learn online and Learn to code in JavaScript, the native language of the web used by developers the world over.</p>'
+    },
+    {
+        title: 'React Development Remote',
+        desc: '<p>Learn online and start building maintainable web applications that can handle constantly changing data at scale. Expand your programming toolkit, and discover why both developers and employers favor this robust JavaScript library. </p>'
+    },
+    {
+        title: 'iOS Development Immersive',
+        desc: '<p>Create App Store-ready products by gaining key industry knowledge and specialized skills in Swift, Xcode, mobile design, and more. </p>'
+    },
+    {
+        title: 'Android Development Immersive',
+        desc: '<p>Join our Android Development Immersive program, and train in the coding, design, and iteration skills essential to starting a career in the sought-after field. </p>'
+    },
+];
+
+function request(){
+ var url = 'https://api.github.com/users/smcurrey528/repos';
+ 
+ fetch(url).then(data => data.json()).then(data => {
+     var repolink = data.map(repo => {
+         console.log(repo.url)
+     })
+ })
+    
+
+}
+
+request()
+
+//POSTING FORM DATA 
+  var testForm = document.getElementById('regForm');
+  testForm.onsubmit = function(event) {
+    event.preventDefault();
+
+    var request = new XMLHttpRequest();
+    // POST to httpbin which returns the POST data as JSON
+    request.open('POST', 'https://external.generalassemb.ly/api/v1/website/leads', /* async = */ false);
+
+    var formData = new FormData(document.getElementById('regForm'));
+
+    request.send(formData);
+
+    console.log(request.response);
+
+}
+
+
+var questionArray = [
+  ["What Do You Want to Accomplish?", "Coding: I want to build dynamic websites and applications.", "Design: I want to create user-friendly digital experiences.", "Data: I want to drive decision-making with accurate analyses.", "Marketing: I want to craft campaign strategies that audiences love.", "Product Management: I want to take the lead, bringing big ideas to life."], 
+  ["How much time can you commit to learning?", "Full Time", "Nights & Weekends", "Part Time"]
+];
+
+//populate document
+for (var i = 0; i < questionArray.length; i++){
+  document.write("<form><span class='question'>" + questionArray[i][0] + "</span><br>");
+  for (var x = 1; x < 4; x++){
+    document.write("<input type='radio' class='answer' name='answer' value='" + questionArray[i][x] + "'>" + questionArray[i][x] + "");
+  }
+  document.write("</form><br>");
+}
+
+var characterAnswer = [
+  [0, 0, 0,'Code'],
+  [0, 0, 1,'Code'],
+  [1, 1, 1,'UX'],
+  [1, 1, 2,'UX'],
+  [2, 2, 2,'Data Science']
+];
+
+//add click check event listeners
+var inputs = document.getElementsByTagName('input');
+for(var i = 0; i < inputs.length; i++){
+  inputs[i].addEventListener('click', check);
+}
+
+var userAnswers = [];
+
+//check questions answers
+function check(){
+  userAnswers = [];
+  var c = 0;
+  for(var i = 0; i < inputs.length; i++){
+    if(inputs[i].checked) {
+      userAnswers.push(i%3);
+      c++;
+    }
+  }
+  if(c==questionArray.length) rate();
+}
+
+//rate the answers per char
+function rate(){
+  console.log(userAnswers);
+  for(var i = 0; i < userAnswers.length; i++){
+    for(var j = 0; j < characterAnswer.length; j++){
+      characterAnswer[j][4] = 0;
+      for(var x = 0; x < 4; x++){
+        if(userAnswers[i] == characterAnswer[j][x])
+          characterAnswer[j][4]++;
+      }
+    }
